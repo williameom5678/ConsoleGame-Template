c02e9934e8c2b252a0d87e93c60e9446232d4a93
williameom <williameom5678@gmail.com>
2023-02-08 10:42:28 +0900

    https://huiyu.tistory.com/entry/C%EC%96%B8%EC%96%B4%EB%A1%9C-%EA%B2%8C%EC%9E%84%EB%A7%8C%EB%93%A4%EA%B8%B0-5-%EC%B2%AB%EB%B2%88%EC%A7%B8-%EA%B2%8C%EC%9E%843%EC%9B%80%EC%A7%81%EC%9D%B4%EB%8A%94-%EA%B3%A8%EB%8C%80-%EB%A7%8C%EB%93%A4%EA%B8%B0

--- main.c
+++ main.c
@@ -14,6 +14,9 @@ FPSData* fpsData;
 void init() {
 	InitFPSData(&fpsData);
 
+	g_Player.center.x = 4;
+	g_Player.center.y = 0;
+
 	g_Player.position.y = 22;
 	g_Player.position.x = 0;
 
@@ -21,9 +24,28 @@ void init() {
 
 	g_Player.strPlayer = (char*)malloc(sizeof(char) * g_Player.nLength);
 	strcpy(g_Player.strPlayer, PLAYER_STR);
+
+	g_Ball.isReady = 1;
+	g_Ball.position.x = g_Player.position.x;
+	g_Ball.position.y = g_Player.position.y - 1;
+	g_Ball.moveTime = 100;
 }
 
-void update() {}
+void update() {
+	clock_t CurTime = clock();
+	if (g_Ball.isReady == 0) {
+		if ((CurTime - g_Ball.oldTime) > g_Ball.moveTime) {
+			if (g_Ball.position.y > 0) {
+				g_Ball.position.y--;
+				g_Ball.oldTime = CurTime;
+			} else {
+				g_Ball.isReady = 1;
+				g_Ball.position.x = g_Player.position.x;
+				g_Ball.position.y = g_Player.position.y - 1;
+			}
+		}
+	}
+}
 
 void render() {
 	ScreenClear();
@@ -32,12 +54,27 @@ void render() {
 
 	char string[100] = { 0, };
 
-	ScreenPrint(g_Player.position.x, g_Player.position.y, g_Player.strPlayer);
+	int printX = g_Player.position.x - g_Player.center.x;
+	//ScreenPrint(printX, g_Player.position.y, g_Player.strPlayer);
+
+	if (printX < 0) {
+		ScreenPrint(0, g_Player.position.y, &g_Player.strPlayer[printX * -1]);
+	} else if (g_Player.position.x + g_Player.center.x + 1 > 40) {
+		strncat(string, g_Player.strPlayer, g_Player.nLength - ((g_Player.position.x + g_Player.center.x + 1) - 40));
+		ScreenPrint(printX, g_Player.position.y, string);
+	} else {
+		ScreenPrint(printX, g_Player.position.y, g_Player.strPlayer);
+	}
+
+	//ScreenPrint(g_Player.position.x, g_Player.position.y, g_Player.strPlayer);
+
+	ScreenPrint(g_Ball.position.x, g_Ball.position.y, "◎");
 
 	sprintf(string, "주인공 이동좌표 : %d, %d", g_Player.position.x, g_Player.position.y);
 	ScreenPrint(0, 3, string);
 
 	// ae
+	//ScreenClear();
 	ScreenFlipping();
 }
 
@@ -53,26 +90,40 @@ int getKeyEvent() {
 void keyProcess(int key) {
 	int remain;
 	switch (key) {
-	case 'a':
-		//break;
+	case 'j':
 		g_Player.position.x--;
-	case 'd':
-		//break;
+		if (g_Player.position.x - g_Player.center.x < 0 || g_Player.position.x + (g_Player.center.x * 2) + 1 >40) {
+			g_Player.position.x--;
+		}
+		break;
+	case 'l':
 		g_Player.position.x++;
-	case 'w':
-		g_Player.position.y--;
-	case 's':
-		g_Player.position.y++;
+		if (g_Player.position.x + (g_Player.center.x * 2) + 1 > 40 || g_Player.position.x - g_Player.center.x < 0) {
+			g_Player.position.x++;
+		}
+		break;
+	case'k':
+		if (g_Ball.isReady)
+		{
+			g_Ball.position.x = g_Player.position.x;
+			g_Ball.position.y = g_Player.position.y - 1;
+			g_Ball.oldTime = clock();
+			g_Ball.isReady = 0;
+		}
+		break;
+	}
+	if (g_Ball.isReady) {
+		g_Ball.position.x = g_Player.position.x;
+		g_Ball.position.y = g_Player.position.y - 1;
 	}
 }
 
 void renderDelay(clock_t WasTime) {
 	clock_t CurTime;
-	while(1) {
+	for (;;) {
 		CurTime = clock();
-		if (CurTime - WasTime > _FR) {
+		if (CurTime - WasTime > _FR)
 			break;
-		}
 	}
 }
 
--- player.h
+++ player.h
@@ -7,8 +7,19 @@ typedef struct Position
 typedef struct
 {
 	Position position;
+	Position center;// 중심 좌표
 	char* strPlayer;// = "└─●─┘";//주인공 캐릭터
 	int nLength; //주인공 캐릭터 전체 길이
 }Player;
 
 Player g_Player;
+
+typedef struct Ball
+{
+	int isReady;
+	Position position;
+	clock_t moveTime;
+	clock_t oldTime;
+}Ball;
+
+Ball g_Ball;
